175a849d713b8176b6e0d0940fc14cf7166ddedc
Chiara <chiaracampagnola@gmail.com>
2021-04-18 12:53:03 +0100

    reorg slow SMO

--- optimisation/smo.py
+++ optimisation/smo.py
@@ -11,9 +11,61 @@ class SMO:
         self.max_passes = max_passes
         self.speed = speed
         
+        # For fast SMO
+        # Flag to indicate whether we are selecting alpha_1 among ALL alphas that break KKT
+        # or only the restricted ones
+        # Restricted alphas = alphas that break KKT AND are not at the boundary (0 or C)
+        self.restricted_loop = False
+        
     def fit(self):
         if self.speed == 'slow':
             return self.smo_slow()
+        
+    def optimise_alphas(self, alpha, b, kernel, i, j, E_i, E_j):
+        changed_alpha = False
+        # Save old alphas
+        alpha_i_old, alpha_j_old = alpha[i], alpha[j]
+
+        # Compute L and H (bound on alpha_j)
+        if self.train_y[i] == self.train_y[j]:
+            L = np.max([0, alpha[i] + alpha[j] - self.C])
+            H = np.min([self.C, alpha[i] + alpha[j]])
+        else:
+            L = np.max([0, alpha[j] - alpha[i]])
+            H = np.max([self.C, self.C + alpha[j] - alpha[i]])
+        
+        if L != H:
+            # Compute eta
+            eta = 2*kernel[i,j] - kernel[i,i] - kernel[j,j]
+
+            if eta < 0:
+                # Compute new alpha_j and clip
+                alpha_j = alpha_j_old - (self.train_y[j] * (E_i - E_j))/eta
+                if alpha_j > H:
+                    alpha_j = H
+                elif alpha_j < L:
+                    alpha_j = L
+                
+                alpha[j] = alpha_j
+
+                # Only continue if alpha_j has changed "significantly"
+                if np.abs(alpha_j - alpha_j_old) > 1e-5:
+                    # Compute new alpha_i
+                    alpha[i] = alpha_i_old + self.train_y[i] * self.train_y[j] * (alpha_j_old - alpha_j)
+
+                    # Compute new threshold
+                    b1 = b - E_i - self.train_y[i] * (alpha[i] - alpha_i_old) * kernel[i,i] - self.train_y[j] * (alpha[j] - alpha_j_old) * kernel[i,j]
+                    b2 = b - E_j - self.train_y[i] * (alpha[i] - alpha_i_old) * kernel[i,j] - self.train_y[j] * (alpha[j] - alpha_j_old) * kernel[j,j]
+
+                    if alpha[i] > 0 and alpha[i] < self.C:
+                        b = b1
+                    elif alpha[j] > 0 and alpha[j] < self.C:
+                        b - b2
+                    else:
+                        b = (b1+b2)/2
+                    
+                    changed_alpha = True
+        return alpha, b, changed_alpha
 
     def smo_slow(self):
         num_training_examples = self.train_y.shape[0]
@@ -29,6 +81,129 @@ class SMO:
         
         while passes < self.max_passes:
             changed_alpha = False
+            for i in range(num_training_examples): # Iterate through alphas
+
+                ########## Check if alpha_i violates KKT conditions ########
+                pred_y_i = predict(alpha, kernel[:,i], b, self.train_y)
+                E_i = pred_y_i - self.train_y[i] # Error
+
+                if (self.train_y[i]*E_i < -self.tol and alpha[i] < self.C) or (self.train_y[i]*E_i > -self.tol and alpha[i] > 0): # KKT conditions are broken
+
+                    # Pick alpha_j != alpha_i at random
+                    j = np.random.randint(0, num_training_examples)
+                    while j == i:
+                        j = np.random.randint(0, num_training_examples)
+
+                    # Calculate error 2
+                    pred_y_j = predict(alpha, kernel[:,j], b, self.train_y)
+                    E_j = pred_y_j - self.train_y[j]
+                    
+                    alpha, b, changed_alpha = self.optimise_alphas(alpha, b, kernel, i, j, E_i, E_j)
+                    
+                    g_iterates.append(calculate_g(alpha, Q_matrix(self.train_X, self.train_y, self.kernel_fun)))
+                    
+            if not changed_alpha:
+                passes += 1
+                
+        # Find support vectors so we keep only elements corresponding to suport vectors
+        SV = alpha > 0
+        
+        result = {'train_X': self.train_X[SV,:],
+                'train_y': self.train_y[SV],
+                'alpha': alpha[SV],
+                'b': b,
+                'g': g_iterates}
+        
+        return result
+
+    def candidate_alphas(self, alpha, kernel, b):
+        # Return two sets:
+        #   - alphas breaking KKT
+        #   - non-boundary alphas breaking  KKT
+        
+        self.E = {}
+        
+        candidates = set()
+        non_bound_candidates = set()
+        
+        num_training_examples = self.train_y.shape[0]
+
+        for i in range(num_training_examples): # Iterate through alphas
+
+            pred_y_i = predict(alpha, kernel[:,i], b, self.train_y)
+            self.E[i] = pred_y_i - self.train_y[i] # Error
+            
+
+            # Check it breaks KKT
+            if (self.train_y[i]*self.E[i] < -self.tol and alpha[i] < self.C) or (self.train_y[i]*self.E[i] > -self.tol and alpha[i] > 0):
+                candidates.add(i)
+                
+                # Check it's non-bound
+                if alpha[i] != 0 and alpha[i] != self.C:
+                    non_bound_candidates.add(i)
+                
+        return candidates, non_bound_candidates
+    
+    def find_alpha_2(self, i, kernel, b, non_bound_alphas):
+        pred_y_i = predict(alpha, kernel[:,i], b, self.train_y)
+        E_i = pred_y_i - self.train_y[i] # Error
+        
+        if E_i > 0:
+            min_E = 100000000
+            best_j = 0
+            for j in non_bound_alphas:
+                if E[j] < min_E and j!=i:
+                    min_E = E[j]
+                    best_j = j
+        else:
+            max_E = -1000000
+            best_j = 0
+            for j in non_bound_alphas:
+                if E[j] > max_E and j!=i:
+                    max_E = E[j]
+                    best_j = j
+        return best_j
+
+    def smo_fast(self):
+        num_training_examples = self.train_y.shape[0]
+
+        # Initialise
+        alpha = np.zeros(num_training_examples)
+        b = 0
+        passes = 0
+        
+        kernel = self.kernel_fun(self.train_X, self.train_X)
+        
+        g_iterates = []
+        
+        while passes < self.max_passes:
+            changed_alpha = False
+            
+            # Create two lists of candidates: all KKT breaking, and non-boundary
+            candidate_alphas, candidate_non_boundary_alphas = self.candidate_alphas(alpha, kernel, b)
+            
+            if not self.restricted_loop:
+                # Iterate through all KKT breaking alphas -> alphas
+                for i in candidate_alphas:
+                    j = find_alpha_2(i, kernel, b, non_bound_alphas)
+                    
+                    
+                self.restricted_loop = True # Go to restricted loop
+            else:
+                # Keep going through all KKT breaking alphas
+                # not at boundary until there are no more -> non_boundary_alphas
+                while candidate_non_boundary_alphas:
+                    pass
+                
+                # Find set of alphas
+                
+                # While the set is not empty keep going through it
+                
+                self.restricted_loop = False # Go back to larger loop
+            
+            
+            
+            
             for i in range(num_training_examples): # Iterate through alphas
 
                 ########## Check if alpha_i violates KKT conditions ########
@@ -110,100 +285,3 @@ class SMO:
                 'g': g_iterates}
         
         return result
-
-def smo_fast(tol, C, kernel_fun, train_X, train_y, max_passes):
-    num_training_examples = train_y.shape[0]
-
-    # Initialise
-    alpha = np.zeros(num_training_examples)
-    b = 0
-    passes = 0
-    
-    kernel = kernel_fun(train_X, train_X)
-    
-    g_iterates = []
-    
-    while passes < max_passes:
-        changed_alpha = False
-        for i in range(num_training_examples): # Iterate through alphas
-
-            ########## Check if alpha_i violates KKT conditions ########
-            pred_y_i = predict(alpha, kernel[:,i], b, train_y)
-            E_i = pred_y_i - train_y[i] # Error
-
-            if (train_y[i]*E_i < -tol and alpha[i] < C) or (train_y[i]*E_i > -tol and alpha[i] > 0): # KKT conditions are broken
-
-                # Pick alpha_j != alpha_i at random
-                j = np.random.randint(0, num_training_examples)
-                while j == i:
-                    j = np.random.randint(0, num_training_examples)
-
-                # Calculate error 2
-                pred_y_j = predict(alpha, kernel[:,j], b, train_y)
-                E_j = pred_y_j - train_y[j]
-
-                # Save old alphas
-                alpha_i_old, alpha_j_old = alpha[i], alpha[j]
-
-                # Compute L and H (bound on alpha_j)
-                if train_y[i] == train_y[j]:
-                    L = np.max([0, alpha[i] + alpha[j] - C])
-                    H = np.min([C, alpha[i] + alpha[j]])
-                else:
-                    L = np.max([0, alpha[j] - alpha[i]])
-                    H = np.max([C, C + alpha[j] - alpha[i]])
-                
-                if L == H:
-                    continue
-
-                # Compute eta
-                eta = 2*kernel[i,j] - kernel[i,i] - kernel[j,j]
-
-                if eta >= 0:
-                    continue
-                
-                # Compute new alpha_j and clip
-                alpha_j = alpha_j_old - (train_y[j] * (E_i - E_j))/eta
-                if alpha_j > H:
-                    alpha_j = H
-                elif alpha_j < L:
-                    alpha_j = L
-                
-                alpha[j] = alpha_j
-
-                # If alpha_j has not changed "significantly", end iteration
-                if np.abs(alpha_j - alpha_j_old) < 1e-5:
-                    continue
-
-                # Compute new alpha_i
-                alpha[i] = alpha_i_old + train_y[i] * train_y[j] * (alpha_j_old - alpha_j)
-
-                # Compute new threshold
-                b1 = b - E_i - train_y[i] * (alpha[i] - alpha_i_old) * kernel[i,i] - train_y[j] * (alpha[j] - alpha_j_old) * kernel[i,j]
-                b2 = b - E_j - train_y[i] * (alpha[i] - alpha_i_old) * kernel[i,j] - train_y[j] * (alpha[j] - alpha_j_old) * kernel[j,j]
-
-                if alpha[i] > 0 and alpha[i] < C:
-                    b = b1
-                elif alpha[j] > 0 and alpha[j] < C:
-                    b - b2
-                else:
-                    b = (b1+b2)/2
-                
-                changed_alpha = True
-                
-                g_iterates.append(calculate_g(alpha, Q_matrix(train_X, train_y, kernel_fun)))
-                
-        if not changed_alpha:
-            passes += 1
-            
-    # Find support vectors so we keep only elements corresponding to suport vectors
-    SV = alpha > 0
-    
-    result = {'train_X': train_X[SV,:],
-              'train_y': train_y[SV],
-              'alpha': alpha[SV],
-              'b': b,
-              'g': g_iterates}
-    
-    return result
-
